b2993d44a0c1cb6e1e252e71db7197d4df169138
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-02-13 10:30:36 +0500

    Create index.tsx

---
+++ packages/nextjs/pages/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import type { NextPage } from "next";
+import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { MetaHeader } from "~~/components/MetaHeader";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <MetaHeader />
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <h1 className="text-center mb-8">
+            <span className="block text-2xl mb-2">Welcome to</span>
+            <span className="block text-4xl font-bold">Escrow System</span>
+          </h1>
+          
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default Home;
